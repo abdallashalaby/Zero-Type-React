f3ad7978cb2b7d7c1c69a7ef6da8108bb073481d
abdalla shalaby <abdallashalaby11@gmail.com>
2022-05-23 15:38:04 +0200

    added part of footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Header from "./Header"
 import About from "./About"
+import Footer from "./footer"
 import "./App.css"
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
     <Header />
     <About />
+    <Footer />
     </div>
     )
 }
