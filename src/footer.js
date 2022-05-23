b2e1dedd261a8a58178fffaf5b30ebf960273c90
abdalla shalaby <abdallashalaby11@gmail.com>
2022-05-23 15:38:47 +0200

    added part of footer

--- src/footer.js
+++ src/footer.js
@@ -1,16 +1,12 @@
 import React from "react"
 import "./footer.css"
 import fb from "./facebook-square-brands.svg"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import
 
 function Footer() {
     return (
       <div id="footer">
         <div>© 2023 StarTech.All Rights reserved</div>
         <div className="icons">
-<FontAwesomeIcon icon={brands('facebook')} />
-
         </div>
       </div>
     )
